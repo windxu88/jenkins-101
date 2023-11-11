2c6a70d9ba149c583b0027685b6699e5fa5b0c77
wind.xu <wind.xu@unity3d.com>
2023-11-11 10:54:12 -0500

    Fix a typo

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,6 @@ import sys, os
 
 print(sys.version)
 print("Hello world")
-print("Build job ID is: {0}\n Build URL is: {1}\n".format(os.environ['BUILD_ID'], os.environ['BUILD.URL']))
+print("Build job ID is: {0}\n Build URL is: {1}\n".format(os.environ['BUILD_ID'], os.environ['BUILD_URL']))
 f = open('test.tx', 'w')
 f.write('sssss')
